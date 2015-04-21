bdb8ad1eb875d076627255e8b75c903f9f8485c5
Alex Browne <stephenalexbrowne@gmail.com>
2015-04-21 00:12:22 -0400

    Move config doc comments so they will actually show up in godoc.org (hopefully)

--- config.go
+++ config.go
@@ -13,18 +13,9 @@ type configType struct {
 
 // databaseConfig holds config variables specific to the database
 type databaseConfig struct {
-	// Address is the address of the redis database to connect to. Default is
-	// "localhost:6379".
-	Address string
-	// Network is the type of network to use to connect to the redis database
-	// Default is "tcp".
-	Network string
-	// Database is the redis database number to use for storing all data. Default
-	// is 0.
+	Address  string
+	Network  string
 	Database int
-	// Password is a password to use for connecting to a redis database via the
-	// AUTH command. If empty, Jobs will not attempt to authenticate. Default is
-	// "" (an empty string).
 	Password string
 }
 
@@ -33,9 +24,18 @@ type databaseConfig struct {
 // of your program.
 var Config = configType{
 	Db: databaseConfig{
-		Address:  "localhost:6379",
-		Network:  "tcp",
+		// Address is the address of the redis database to connect to. Default is
+		// "localhost:6379".
+		Address: "localhost:6379",
+		// Network is the type of network to use to connect to the redis database
+		// Default is "tcp".
+		Network: "tcp",
+		// Database is the redis database number to use for storing all data. Default
+		// is 0.
 		Database: 0,
+		// Password is a password to use for connecting to a redis database via the
+		// AUTH command. If empty, Jobs will not attempt to authenticate. Default is
+		// "" (an empty string).
 		Password: "",
 	},
 }

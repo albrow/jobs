ba667a3e715c8c1e98a6c14a4159e8fa96f628b0
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-20 00:07:31 -0400

    Add documentation to FindById about ErrorJobNotFound

--- job.go
+++ job.go
@@ -412,7 +412,8 @@ func (t *transaction) scanJobById(id string, job *Job) {
 }
 
 // FindById returns the job with the given id or an error if the job cannot be found
-// or there was a problem connecting to the database.
+// (in which case the error will have type ErrorJobNotFound) or there was a problem
+// connecting to the database.
 func FindById(id string) (*Job, error) {
 	job := &Job{}
 	t := newTransaction()
